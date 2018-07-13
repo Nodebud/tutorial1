71b4933438cbf3e6627b18160b8364bc186d3e1b
Unknown <Bryan@Nodebud.com>
2018-07-13 09:35:05 -0500

    Initial commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Bryan Azorsky on 7/12/18.
+//  Copyright © 2018 Bryan Azorsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            
+            myLabel.text = "Bryan Can Program"
+        }
+        
+
+    
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
