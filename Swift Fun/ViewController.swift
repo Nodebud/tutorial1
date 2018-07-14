88ddb7207f5ed34dbf1fd1361efec2c5502b4b1e
Bryan Azorsky <Bryan@Nodebud.com>
2018-07-14 09:05:33 -0500

    added the addition switch

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,25 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var buttonCount = 0
+//    var buttonCount = 0
     @IBOutlet weak var myLabel: UILabel!
-    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
     @IBAction func buttonTapped(_ sender: Any) {
+
+        let addition = additionSwitch.isOn
         
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        }
+        
+        else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+
+        
+        
+        /*
         buttonCount = buttonCount + 1
         
-        print(buttonCount)
+        print("The button has been pushed \(buttonCount)")
+        
         
         if buttonCount >= 10 {
             view.backgroundColor = UIColor.red
             
             myLabel.text = "Bryan Can Program"
         }
-        
-
-    
-    }
-    
+        */
+  
+     }
     
     
     override func viewDidLoad() {
